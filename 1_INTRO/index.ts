831a212f75dba8e88acaea4c97431bd2f6b2ec46
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-10-06 13:52:29 -0300

    first commit

---
+++ 1_INTRO/index.ts
@@ -0,0 +1,11 @@
+const firstName = 'Álisson';
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+    console.log("Olá " + name)
+}
+
+greeting(firstName);
+/* greeting(anotherName);
+greeting(x); */
