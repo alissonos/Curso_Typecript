470859900bada142df41f4792abdbc8af69ffe2e
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-10-09 10:59:53 -0300

    Tipo String

--- 2_FUNDAMENTOS/index.js
+++ 2_FUNDAMENTOS/index.js
@@ -7,3 +7,11 @@ var y = 15.6446546;
 console.log(typeof y);
 console.log(y);
 console.log(y.toPrecision(3));
+// 2 - string 
+var firstName = "Álisson";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Oliveira";
+fullName = firstName + " " + lastName;
+console.log(typeof fullName);
+console.log(fullName);
--- 2_FUNDAMENTOS/index.ts
+++ 2_FUNDAMENTOS/index.ts
@@ -14,3 +14,36 @@ console.log(typeof y)
 console.log(y)
 
 console.log(y.toPrecision(3))
+
+// 2 - string 
+
+const firstName: string = "Álisson";
+
+console.log(firstName.toUpperCase());
+
+let fullName: string
+
+const lastName: string = "Oliveira"
+
+fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+console.log(typeof fullName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
