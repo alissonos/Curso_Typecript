09a7b775f605188e24e8770e5ee0c965d30a46a8
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-10-16 23:05:50 -0300

    Array

---
+++ 3_AVANÇANDO_EM_TIPOS/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// 1 - Arrays
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers);
+console.log(numbers[2]);
+const nomes = ["Álisson", "Marília", "Artur"];
+console.log(nomes);
+console.log(nomes[1]);
---
+++ 3_AVANÇANDO_EM_TIPOS/index.ts
@@ -0,0 +1,12 @@
+// 1 - Arrays
+let numbers: number[] = [1, 2, 3]
+
+numbers.push(5)
+
+console.log(numbers)
+console.log(numbers[2])
+
+const nomes: string[] = ["Álisson", "Marília", "Artur"]
+
+console.log(nomes)
+console.log(nomes[1])
