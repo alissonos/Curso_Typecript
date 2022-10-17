5e06e0f56d12ee1a270132cab956707b71b71c69
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-10-16 23:50:52 -0300

    Outra sintaxe de Array

--- 3_AVANÇANDO_EM_TIPOS/index.js
+++ 3_AVANÇANDO_EM_TIPOS/index.js
@@ -7,3 +7,9 @@ console.log(numbers[2]);
 const nomes = ["Álisson", "Marília", "Artur"];
 console.log(nomes);
 console.log(nomes[1]);
+// 2 - Outra sintaxe Array
+const nums = [1, 2, 3, 4];
+nums.push(200);
+console.log(nums);
+console.log(nums.length);
+console.log(nums.splice(1));
--- 3_AVANÇANDO_EM_TIPOS/index.ts
+++ 3_AVANÇANDO_EM_TIPOS/index.ts
@@ -10,3 +10,14 @@ const nomes: string[] = ["Álisson", "Marília", "Artur"]
 
 console.log(nomes)
 console.log(nomes[1])
+
+// 2 - Outra sintaxe Array
+const nums: Array<number> = [1, 2, 3, 4]
+
+nums.push(200)
+
+console.log(nums)
+console.log(nums.length)
+console.log(nums.splice(1))
+
+
